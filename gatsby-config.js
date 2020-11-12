f34b3f4042d1be2dd143190b47ed6f8c5e2892f3
Adrian Grimm <usmcamgrimm@gmail.com>
2020-11-12 06:07:37 -0500

    Good job dumbass

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,7 +5,7 @@ module.exports = {
     author: `@gatsbyjs`,
   },
   plugins: [
-    `gatsby-plugin-lesss`,
+    `gatsby-plugin-less`,
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
