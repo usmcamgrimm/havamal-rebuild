27a545f020f4ac0c93ab6376f23f9501d5dc57ae
Adrian Grimm <usmcamgrimm@gmail.com>
2020-11-15 12:26:19 -0500

    Still trying to center this on a larger screen

--- src/components/container/container.js
+++ src/components/container/container.js
@@ -2,5 +2,11 @@ import React from "react"
 import ContainerStyle from "./container.module.less"
 
 export default function Container({ children }) {
-  return <div className={ContainerStyle.container}>{children}</div>
+  return (
+    <div className={ContainerStyle.container}>
+      <div className={ContainerStyle.welcomeWrapper}>
+        {children}
+      </div>
+    </div>
+  )
 }
