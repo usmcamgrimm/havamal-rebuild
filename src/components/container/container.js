e2deba840689750d308f24369d970dad07f4d8cc
Adrian Grimm <usmcamgrimm@gmail.com>
2020-11-15 12:27:34 -0500

    Should have just done a revert

--- src/components/container/container.js
+++ src/components/container/container.js
@@ -4,9 +4,7 @@ import ContainerStyle from "./container.module.less"
 export default function Container({ children }) {
   return (
     <div className={ContainerStyle.container}>
-      <div className={ContainerStyle.welcomeWrapper}>
         {children}
-      </div>
     </div>
   )
 }
