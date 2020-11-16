3fba33fda2183f47b9add07ef22b4eb22c79fdf2
Adrian Grimm <usmcamgrimm@gmail.com>
2020-11-16 06:21:24 -0500

    Revert "Add a container for the logo and h1 elements on main page"
    This reverts commit a8e097e2188e9f190ebb86f9a9241b9a93a398cc.

--- src/components/container/container.js
+++
@@ -1,6 +0,0 @@
-import React from "react"
-import ContainerStyle from "./container.module.less"
-
-export default function Container({ children }) {
-  return <div className={ContainerStyle.container}>{children}</div>
-}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,5 @@
 import React from "react"
 import '../styles/style.less'
-import Container from "../components/container/container"
 
 import Logo from "../components/logo/logo"
 import SEO from "../components/seo"
@@ -8,10 +7,8 @@ import SEO from "../components/seo"
 const HavamalHome = () => (
   <>
     <SEO title="Home" />
-    <Container>
-      <Logo />
-      <h1>Hail, Hordes of Havamal!</h1>
-    </Container>
+    <Logo />
+    <h1>Hail, Hordes of Havamal!</h1>
   </>
 )
 
