c768c164dad1928e758978e5d9000e8338ebefae
Adrian Grimm <usmcamgrimm@gmail.com>
2020-11-16 06:23:35 -0500

    Revert "Good job dumbass"
    This reverts commit f34b3f4042d1be2dd143190b47ed6f8c5e2892f3.

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,7 +5,7 @@ module.exports = {
     author: `@gatsbyjs`,
   },
   plugins: [
-    `gatsby-plugin-less`,
+    `gatsby-plugin-lesss`,
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
