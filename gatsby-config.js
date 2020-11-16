dcfe1a93f8875559a0161bec99122fc6a98c1cfa
Grimm <Adrian.Grimm@securitases.com>
2020-11-16 12:33:40 -0500

    Update for SEO

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `HAIL HAVAMAL`,
+    description: `Havamal is a band hailing from Sweden, with musical themes inspired by the old ways of the Norse. The hymn-weavers spin tales of the Gods and of Vikings, set in crushing melodic death metal. Discography includes Call of the North and Tales From Yggdrasil.`,
   },
   plugins: [
     `gatsby-plugin-postcss`,
@@ -19,13 +18,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `HAIL HAVAMAL`,
+        short_name: `HAVAMAL`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#0e181f`,
+        theme_color: `#0e181f`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/resources/icons/tabIcon.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
