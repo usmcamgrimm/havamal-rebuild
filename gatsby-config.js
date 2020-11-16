c74b708f258f67f00ad465b8836f0c48aa9ddead
Adrian Grimm <usmcamgrimm@gmail.com>
2020-11-16 18:17:34 -0500

    Fix the sitemetadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,6 +2,7 @@ module.exports = {
   siteMetadata: {
     title: `HAIL HAVAMAL`,
     description: `Havamal is a band hailing from Sweden, with musical themes inspired by the old ways of the Norse. The hymn-weavers spin tales of the Gods and of Vikings, set in crushing melodic death metal. Discography includes Call of the North and Tales From Yggdrasil.`,
+    author: `Adrian Grimm`,
   },
   plugins: [
     `gatsby-plugin-postcss`,
