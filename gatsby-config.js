4e14cde72d6f8c79d43e11819f39c448836e966b
Adrian Grimm <usmcamgrimm@gmail.com>
2020-11-16 06:50:53 -0500

    Getting started with Tailwind CSS

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,7 +5,7 @@ module.exports = {
     author: `@gatsbyjs`,
   },
   plugins: [
-    `gatsby-plugin-lesss`,
+    `gatsby-plugin-postcss`,
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
