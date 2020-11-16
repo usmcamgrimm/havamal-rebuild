f862eed3fb45dfcc90294571754013d67bd9de5e
Adrian Grimm <usmcamgrimm@gmail.com>
2020-11-16 06:22:49 -0500

    Revert "Get some work started"
    This reverts commit 8ebe728ed2dd23a320035f4ad667fe1e31de1523.

--- src/components/image.js
+++ src/components/image.js
@@ -16,7 +16,7 @@ import Img from "gatsby-image"
 const Image = () => {
   const data = useStaticQuery(graphql`
     query {
-      placeholderImage: file(relativePath: { eq: "resources/images/bg2.jpeg" }) {
+      placeholderImage: file(relativePath: { eq: "gatsby-astronaut.png" }) {
         childImageSharp {
           fluid(maxWidth: 300) {
             ...GatsbyImageSharpFluid
