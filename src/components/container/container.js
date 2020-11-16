83ee96d967869e25354eaf055ab8b622d3211482
Adrian Grimm <usmcamgrimm@gmail.com>
2020-11-16 06:19:58 -0500

    Revert "Should have just done a revert"
    This reverts commit e2deba840689750d308f24369d970dad07f4d8cc.

--- src/components/container/container.js
+++ src/components/container/container.js
@@ -4,7 +4,9 @@ import ContainerStyle from "./container.module.less"
 export default function Container({ children }) {
   return (
     <div className={ContainerStyle.container}>
+      <div className={ContainerStyle.welcomeWrapper}>
         {children}
+      </div>
     </div>
   )
 }
