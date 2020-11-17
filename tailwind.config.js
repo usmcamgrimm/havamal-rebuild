5395e6b0907cc2a422a193549b61c19868a590cb
Adrian Grimm <usmcamgrimm@gmail.com>
2020-11-17 06:45:30 -0500

    Update default tailwind color themes

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,17 @@ module.exports = {
   },
   purge: ["./src/**/*.js", "./src/**/*.jsx", "./src/**/*.ts", "./src/**/*.tsx"],
   theme: {
-    extend: {},
+    colors: {
+      yellow: `#ffcd00`,
+      blue: {
+        lighter: `#66add9`,
+        default: `#004b87`,
+      },
+      red: {
+        default: `#ce1126`,
+        darker: `#d50000`,
+      }
+    },
   },
   variants: {},
   plugins: [],
