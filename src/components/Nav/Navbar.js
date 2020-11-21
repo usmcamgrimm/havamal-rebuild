b264eaeb1bdb6621020495992acefc8c015eeee1
Adrian Grimm <usmcamgrimm@gmail.com>
2020-11-21 11:32:24 -0500

    Added a gradient for the nav

--- src/components/Nav/Navbar.js
+++ src/components/Nav/Navbar.js
@@ -6,7 +6,7 @@ const Nav = () => {
     const [isExpanded, toggleExpansion] = useState(false)
   
     return (
-      <nav className="flex items-center justify-evenly flex-wrap bg-blue p-6">
+      <nav className="flex items-center justify-evenly flex-wrap from-transparent via-blue to-transparent bg-gradient-to-r p-6">
         <div className="block lg:hidden">
           <button onClick={() => toggleExpansion(!isExpanded)} className="flex items-center px-3 py-2 text-yellow">
             <svg className="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
