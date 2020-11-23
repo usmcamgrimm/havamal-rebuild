15e290bff32629f4635bce3e6d03e1084bffd4cc
Adrian Grimm <usmcamgrimm@gmail.com>
2020-11-23 12:01:12 -0500

    Remove duplicate

--- gatsby-config.js
+++ gatsby-config.js
@@ -50,16 +50,6 @@ module.exports = {
         apiKey: process.env.CLOUDINARY_API_KEY,
         apiSecret: process.env.CLOUDINARY_API_SECRET,
         resourceType: `image`,
-        prefix: `havamal`
-      }
-    },
-    {
-      resolve: `gatsby-source-cloudinary`,
-      options: {
-        cloudName: process.env.CLOUDINARY_CLOUD_NAME,
-        apiKey: process.env.CLOUDINARY_API_KEY,
-        apiSecret: process.env.CLOUDINARY_API_SECRET,
-        resourceType: `image`,
         prefix: `band`
       }
     }
