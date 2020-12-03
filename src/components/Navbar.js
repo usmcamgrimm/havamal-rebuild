358ab7267dc22982e1af061e34c18570c08e8a4d
Adrian Grimm <usmcamgrimm@gmail.com>
2020-12-03 11:47:34 -0500

    Update Navbar component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,7 +8,7 @@ const Nav = () => {
     const [isExpanded, toggleExpansion] = useState(false)
   
     return (
-      <nav className="lg:w-1/2  my-0 mx-auto flex items-center flex-nowrap lg:from-transparent lg:via-blue lg:to-transparent lg:bg-gradient-to-r lg:p-2">
+      <nav className="lg:w-3/4 my-0 mx-auto flex items-center flex-nowrap lg:from-transparent lg:via-blue lg:to-transparent lg:bg-gradient-to-r lg:p-1">
         <div className="block lg:hidden">
           <button onClick={() => toggleExpansion(!isExpanded)} className="flex items-center px-3 py-2 text-yellow">
             <svg className="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
@@ -30,6 +30,9 @@ const Nav = () => {
             </Link>
             <Link to={`/raids`} className="block mt-4 px-4 lg:inline-block lg:mt-0 text-yellow hover:text-white">
               Raids
+            </Link>
+            <Link to={`/news`} className="block mt-4 px-4 lg:inline-block lg:mt-0 text-yellow hover:text-white">
+              News
             </Link>
             <Link to={`/resources`} className="block mt-4 px-4 lg:inline-block lg:mt-0 text-yellow hover:text-white">
               Resources
