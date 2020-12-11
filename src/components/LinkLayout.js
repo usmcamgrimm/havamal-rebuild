97591670ac690a542af5e10fc9609bba0936d91e
Adrian Grimm <usmcamgrimm@gmail.com>
2020-12-11 11:51:10 -0500

    Move the links into their own component
    Adjust padding

---
+++ src/components/LinkLayout.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+import Bandbond from '../resources/icons/bandbond'
+import Bandcamp from '../resources/icons/bandcamp'
+import Facebook from '../resources/icons/facebook'
+import Instagram from '../resources/icons/instagram'
+import Youtube from '../resources/icons/youtube'
+import AGR from '../resources/icons/AGR'
+import Horde from '../resources/icons/horde'
+
+export default function LinkLayout() {
+  return (
+    <div className="flex flex-col justify-between py-6">
+      <div className="flex flex-row justify-center lg:space-x-3 md:space-x-2">
+        <Bandcamp />
+        <Facebook />
+        <Bandbond />
+        <Instagram />
+        <Youtube />
+      </div>
+      <div className="flex flex-row justify-center pt-8 space-x-3">
+        <AGR />
+        <Horde />
+      </div>
+    </div>
+  )
+}
--- src/pages/resources.js
+++ src/pages/resources.js
@@ -2,14 +2,7 @@ import React from 'react'
 
 import SEO from "../components/seo"
 import Layout from '../components/Layout'
-
-import Bandbond from '../resources/icons/bandbond'
-import Bandcamp from '../resources/icons/bandcamp'
-import Facebook from '../resources/icons/facebook'
-import Instagram from '../resources/icons/instagram'
-import Youtube from '../resources/icons/youtube'
-import AGR from '../resources/icons/AGR'
-import Horde from '../resources/icons/horde'
+import LinkLayout from '../components/LinkLayout'
 
 const ResourcesPage = () => (
   <>
@@ -18,19 +11,7 @@ const ResourcesPage = () => (
       <h1 className="text-6xl text-center my-16 font-pirata font-medium text-blue-lighter">
         Resources
       </h1>
-      <div className="flex flex-col justify-between space-y-6">
-        <div className="flex flex-row justify-center lg:space-x-3 md:space-x-2">
-          <Bandcamp />
-          <Facebook />
-          <Bandbond />
-          <Instagram />
-          <Youtube />
-        </div>
-        <div className="flex flex-row justify-center lg:space-x-3 md:space-x-2">
-          <AGR />
-          <Horde />
-        </div>
-      </div>
+    <LinkLayout />
     </Layout>
   </>
 )
