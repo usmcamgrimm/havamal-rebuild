f9d0304d4aa33dae1a65323e27d2e05473962e06
Adrian Grimm <usmcamgrimm@gmail.com>
2020-12-20 00:51:11 -0500

    Work on resource links layout

--- src/components/LinkLayout.js
+++ src/components/LinkLayout.js
@@ -10,7 +10,7 @@ import Horde from '../resources/icons/horde'
 
 export default function LinkLayout() {
   return (
-    <div className="flex flex-col justify-between py-6">
+    <div className="flex flex-col">
       <div className="flex flex-row justify-center space-x-2 lg:space-x-8">
         <Bandcamp />
         <Facebook />
@@ -18,7 +18,7 @@ export default function LinkLayout() {
         <Instagram />
         <Youtube />
       </div>
-      <div className="flex flex-row justify-center pt-8 space-x-3">
+      <div className="center flex flex-col lg:flex-row justify-center items-center pt-8 space-x-3">
         <AGR />
         <Horde />
       </div>
