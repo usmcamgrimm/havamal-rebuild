150d3cbfe72dc7c2bd490cc9b3016ed96f835735
Adrian Grimm <usmcamgrimm@gmail.com>
2020-12-22 06:49:40 -0500

    Update schema and work on a first post

--- havamal/schemas/newsPost.js
+++ havamal/schemas/newsPost.js
@@ -5,8 +5,31 @@ export default {
   fields: [
     {
       name: 'name',
-      title: 'title',
+      title: 'Title',
       type: 'string'
+    },
+    {
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        source: 'name',
+        maxLength: 100,
+      },
+    },
+    {
+      name: 'image',
+      title: 'Image',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    },
+    {
+      name: 'newsItem',
+      title: 'News Item',
+      description: 'Newsworthy posts from Havamal for updates and such',
+      type: 'text',
     }
   ]
 }
