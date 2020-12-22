1501d4104fee541928bcd00aa26d7d680b1217ce
Adrian Grimm <usmcamgrimm@gmail.com>
2020-12-22 06:12:24 -0500

    Remove generated files and add a shell

--- havamal/schemas/schema.js
+++ havamal/schemas/schema.js
@@ -5,10 +5,7 @@ import createSchema from 'part:@sanity/base/schema-creator'
 import schemaTypes from 'all:part:@sanity/base/schema-type'
 
 // We import object and document schemas
-import blockContent from './blockContent'
-import category from './category'
-import post from './post'
-import author from './author'
+import newsPost from './newsPost'
 
 // Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
@@ -19,11 +16,8 @@ export default createSchema({
   types: schemaTypes.concat([
     // The following are document types which will appear
     // in the studio.
-    post,
-    author,
-    category,
+    newsPost,
     // When added to this list, object types can be used as
     // { type: 'typename' } in other document schemas
-    blockContent,
   ]),
 })
