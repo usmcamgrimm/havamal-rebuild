47675af435f36c868c35343535a5eeae895da5e6
Adrian Grimm <usmcamgrimm@gmail.com>
2020-12-23 08:15:46 -0500

    Revert "Tried to qwrite a query to grab posts from Sanity"
    This reverts commit 96759e9a9d32810a35aec2117801a8a6d2a521af.

--- src/pages/news.js
+++ src/pages/news.js
@@ -1,43 +1,17 @@
 import React from 'react'
-import { graphql } from 'gatsby';
 
 import SEO from "../components/seo"
 import Layout from '../components/Layout'
 
-export default function NewsPage({ data }) {
-  const news = data.news.nodes;
+export default function NewsPage() {
   return (
     <>
       <SEO title="News" />
       <Layout>
         <h1 className="text-6xl text-center my-16 font-pirata font-medium text-blue-lighter">
           News
-        </h1>
+      </h1>
       </Layout>
     </>
   )
 }
-
-export const query = graphql`
-  query NewsQuery {
-    news: allSanityNewsPost {
-      nodes {
-        name
-        id
-        slug {
-          current
-        }
-        postDate
-        postAuthor
-        newsItem
-        image {
-          asset {
-            fluid(maxWidth: 400) {
-              ...GatsbySanityImageFluid
-            }
-          }
-        }
-      }
-    }
-  }
-`;
