15e4783a16edce7c143f4bf4a451f6211adb72d3
Adrian Grimm <usmcamgrimm@gmail.com>
2020-12-23 05:59:28 -0500

    Reformat the News Page

--- src/pages/news.js
+++ src/pages/news.js
@@ -3,15 +3,15 @@ import React from 'react'
 import SEO from "../components/seo"
 import Layout from '../components/Layout'
 
-const NewsPage = () => (
-  <>
-    <SEO title="News" />
-    <Layout>
-      <h1 className="text-6xl text-center my-16 font-pirata font-medium text-blue-lighter">
-        News
+export default function NewsPage() {
+  return (
+    <>
+      <SEO title="News" />
+      <Layout>
+        <h1 className="text-6xl text-center my-16 font-pirata font-medium text-blue-lighter">
+          News
       </h1>
-    </Layout>
-  </>
-)
-
-export default NewsPage
+      </Layout>
+    </>
+  )
+}
