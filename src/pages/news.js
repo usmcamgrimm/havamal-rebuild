b985f970869c9c4471f9507985e013f32b86e31f
Adrian Grimm <usmcamgrimm@gmail.com>
2020-12-23 06:43:52 -0500

    Tried to write a query to grab posts from Sanity
    Also added a new component for post display on news page but code is unreachable so far

--- src/pages/news.js
+++ src/pages/news.js
@@ -1,17 +1,43 @@
 import React from 'react'
+import { graphql } from 'gatsby';
 
 import SEO from "../components/seo"
 import Layout from '../components/Layout'
 
-export default function NewsPage() {
+export default function NewsPage({ data }) {
+  const news = data.news.nodes;
   return (
     <>
       <SEO title="News" />
       <Layout>
         <h1 className="text-6xl text-center my-16 font-pirata font-medium text-blue-lighter">
           News
-      </h1>
+        </h1>
       </Layout>
     </>
   )
 }
+
+export const query = graphql`
+  query NewsQuery {
+    news: allSanityNewsPost {
+      nodes {
+        name
+        id
+        slug {
+          current
+        }
+        postDate
+        postAuthor
+        newsItem
+        image {
+          asset {
+            fluid(maxWidth: 400) {
+              ...GatsbySanityImageFluid
+            }
+          }
+        }
+      }
+    }
+  }
+`;
