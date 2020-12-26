f720de87b08c700921f8ad53e246974e192adcd0
Adrian Grimm <usmcamgrimm@gmail.com>
2020-12-26 15:57:20 -0500

    Query the news post images
    Still needs some work to re-size images

--- src/components/NewsPosts.js
+++ src/components/NewsPosts.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Link } from 'gatsby'
+import Img from 'gatsby-image'
 
 function SinglePost({ news }) {
   return <div>
     <Link to={`/post/$(news.slug.current)`}>
-      <h2 className="tex-2xl font-grenze text-blue-lighter text-underline">
+      <h2 className="text-2xl font-grenze text-blue-lighter text-underline">
         {news.name}
       </h2>
+      <Img fluid={news.image.asset.fluid} alt={news.name} />
     </Link>
   </div>
 }
