2658c46fe6e7d2b99c042bf34b338ccf224b8e76
Adrian Grimm <usmcamgrimm@gmail.com>
2020-12-27 12:24:09 -0500

    Use flex properties for posts display

--- src/components/NewsPosts.js
+++ src/components/NewsPosts.js
@@ -8,13 +8,13 @@ function SinglePost({ news }) {
       <h2 className="text-2xl font-grenze text-blue-lighter hover:underline">
         {news.name}
       </h2>
-      <Img fluid={news.image.asset.fluid} alt={news.name} className="max-w-lg" />
+      <Img fluid={news.image.asset.fluid} alt={news.name} className="max-w-xs" />
     </Link>
   </div>
 }
 
 export default function NewsPosts({ news }) {
-  return <div>
+  return <div className="flex flex-row flex-wrap md:justify-center px-4 space-x-4">
     {news.map((news) => (
       <SinglePost key={news.id} news={news} />
     ))}
