013de9ff4581245732587652b2e5f844d65e5d31
Adrian Grimm <usmcamgrimm@gmail.com>
2020-12-30 18:03:54 -0500

    Use styled-components to create a grid.
    Still need to trim the images so they match.

--- src/components/NewsPosts.js
+++ src/components/NewsPosts.js
@@ -1,27 +1,46 @@
 import React from 'react'
 import { Link } from 'gatsby'
 import Img from 'gatsby-image'
+import styled from 'styled-components';
+
+const NewsGrid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+  gap: 2rem;
+  grid-auto-rows: auto auto 200px;
+`;
+const NewsItem = styled.div`
+  display: grid;
+  @supports not (grid-template-rows: subgrid) {
+    --rows: auto auto 1fr;
+  }
+  grid-template-rows: var(--rows, subgrid);
+  grid-row: span 3;
+  gap: 1.5rem;
+`;
 
 function SinglePost({ news }) {
-  return <div className="grid">
+  return <NewsItem>
     <Link to={`/post/$(news.slug.current)`}>
-      <h2 className="text-2xl font-grenze text-blue-lighter hover:underline">
+      <h2 className="text-2xl font-grenze text-blue-lighter hover:underline m-0">
         {news.name}
       </h2>
-      <h2 className="text-2xl font-grenze text-blue-lighter">
+      <h2 className="text-2xl font-grenze text-blue-lighter m-0">
         {news.postAuthor}
       </h2>
       <Img fluid={news.image.asset.fluid} alt={news.name} className="max-w-xs" />
     </Link>
-  </div>
+  </NewsItem>
 }
 
 export default function NewsPosts({ news }) {
   return (
-    <div className="container grid grid-cols-1 lg:grid-cols-3 gap-4 lg:gap-6 justify-center pt-8">
-      {news.map((news) => (
-        <SinglePost key={news.id} news={news} />
-      ))}
+    <div className="container max-w-full px-16 pt-12">
+      <NewsGrid>
+        {news.map((news) => (
+          <SinglePost key={news.id} news={news} />
+        ))}
+      </NewsGrid>
     </div>
   )
 }
