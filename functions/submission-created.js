dcf9edf9510d0f0448d0fab8c9d2504278e3fe1f
Adrian Grimm <usmcamgrimm@gmail.com>
2021-01-01 14:36:12 -0500

    Send form data to sanity

--- functions/submission-created.js
+++ functions/submission-created.js
@@ -1,7 +1,24 @@
+const sanityClient = require("@sanity/client")
+const client = sanityClient({
+  projectId: process.env.GATSBY_SANITY_PROJECT_ID,
+  dataset: process.env.GATSBY_SANITY_PROJECT_DATASET,
+  token: process.env.GATSBY_SANITY_TOKEN,
+  useCDN: false,
+})
+
 exports.handler = async function (event, context, callback) {
   const { payload } = JSON.parse(event.body)
+  const isContactForm = payload.data.formId === "contactForm"
 
-  console.log(payload)
+  if (isContactForm) {
+    const contact = {
+      _type: "contact",
+      name: payload.data.name,
+      email: payload.data.email,
+      message: payload.data.message,
+    }
+    const result = await client.create(contact).catch((err) => console.log(err))
+  }
 
   callback(null, {
     statusCode: 200,
