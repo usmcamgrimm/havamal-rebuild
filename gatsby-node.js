96d8489bc114b5a04f9126de9e3fcd9bafbd1d51
Adrian Grimm <usmcamgrimm@gmail.com>
2021-01-25 12:02:43 -0500

    Let's start working on pages for the Havamal members

--- gatsby-node.js
+++ gatsby-node.js
@@ -27,6 +27,36 @@ async function newsPostPages({ graphql, actions }) {
   })
 }
 
+async function bandMemberPages({ graphql, actions }) {
+  const bandMemberTemplate = path.resolve('./src/templates/HymnWeaver.js');
+  const { data } = await graphql`
+    query {
+      bandmembers: allSanitybandMembers {
+        nodes {
+          name
+          id
+          slug {
+            current
+          }
+        }
+      }
+    }
+  `;
+  data.bandmembers.nodes.forEach((bandmember) => {
+    actions.createPage({
+      component: bandMemberTemplate,
+      path: `/HymnWeavers/${bandmember,slug.current}`,
+      context: {
+        name: bandmember.bandMembers,
+        slug: bandmember.slug.current,
+      }
+    })
+  })
+}
+
 exports.createPages = async function (params) {
-  await newsPostPages(params)
+  await Promise.all([
+    newsPostPages(params),
+    bandMemberPages(params),
+  ]) 
 }
