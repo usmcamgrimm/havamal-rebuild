2e94955f6ff55810ad59af57edd88671707abbd6
Adrian Grimm <usmcamgrimm@gmail.com>
2021-01-29 05:54:19 -0500

    Add SEO component to the Videos page

--- src/pages/video.js
+++ src/pages/video.js
@@ -3,8 +3,11 @@ import SEO from '../components/seo'
 
 export default function VideoPage() {
   return(
-    <div className="font-grenze text-center text-white text-3xl mt-12">
-      Subscribe to our channel for more!
-    </div>
+    <>
+      <SEO title="Videos" />
+      <div className="font-grenze text-center text-white text-3xl mt-12">
+        Subscribe to our channel for more!
+      </div>
+    </>
   )
 }
