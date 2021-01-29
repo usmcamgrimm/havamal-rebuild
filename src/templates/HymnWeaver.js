958e03dd1b056debcb22cbecdf83eea518faf471
Adrian Grimm <usmcamgrimm@gmail.com>
2021-01-29 06:35:40 -0500

    Fix the query to display content on the page

--- src/templates/HymnWeaver.js
+++ src/templates/HymnWeaver.js
@@ -2,19 +2,19 @@ import React from 'react'
 import { graphql } from 'gatsby'
 import Img from 'gatsby-image'
 
-export default function HymnWeaverPage({ data: { bandMembers } }) {
+export default function HymnWeaverPage({ data: { hw } }) {
   return (
     <div className="container">
-      <h2>{bandMembers.name}</h2>
-      <Img fluid={bandMembers.image.asset.fluid} />
-      <p>{bandMembers.description}</p>
+      <h2>{hw.name}</h2>
+      <Img fluid={hw.image.asset.fluid} />
+      <p>{hw.description}</p>
     </div>
   )
 }
 
 export const query = graphql`
   query($slug: String!) {
-    HymnWeaver: sanityBand (slug: { current: { eq: $slug } }) {
+    hw: sanityBand (slug: { current: { eq: $slug } }) {
       name
       id
       bio
