f89d55ab9e926dda9d32218558fb2ee426c26625
Adrian Grimm <usmcamgrimm@gmail.com>
2021-02-08 11:55:07 -0500

    Fix sizing of NewsPost images

--- src/components/NewsPosts.js
+++ src/components/NewsPosts.js
@@ -13,9 +13,9 @@ const NewsItem = styled.div`
   display: grid;
   padding: 2rem;
   text-align: center;
-  img {
-    width: 100%;
-    height: 300px;
+  .gatsby-image-wrapper {
+    width: auto;
+    max-height: 200px;
     object-fit: contain;
     display: grid;
     align-tems: center;
@@ -23,17 +23,19 @@ const NewsItem = styled.div`
 `;
 
 function SinglePost({ news }) {
-  return <NewsItem>
-    <Link to={`/post/${news.slug.current}`}>
-      <h2 className="text-2xl font-grenze text-blue-lighter hover:underline m-0">
-        {news.name}
-      </h2>
-      <h2 className="text-2xl font-grenze text-blue-lighter m-0">
-        {news.postAuthor}
-      </h2>
-      <Img fluid={news.image.asset.fluid} alt={news.name} className="max-w-xs" />
-    </Link>
-  </NewsItem>
+  return (
+    <NewsItem>
+      <Link to={`/post/${news.slug.current}`}>
+        <h2 className="text-2xl font-grenze text-blue-lighter hover:underline m-0">
+          {news.name}
+        </h2>
+        <h2 className="text-2xl font-grenze text-blue-lighter m-0">
+          {news.postAuthor}
+        </h2>
+        <Img fluid={news.image.asset.fluid} alt={news.name} className="max-w-xs" />
+      </Link>
+    </NewsItem>
+  )
 }
 
 export default function NewsPosts({ news }) {
