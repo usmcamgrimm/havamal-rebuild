7e9666232e099704d042524effea44715ccf8868
Adrian Grimm <usmcamgrimm@gmail.com>
2021-02-09 06:08:42 -0500

    Trying to make the logo responsive

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -5,7 +5,7 @@ import Img from "gatsby-image"
 const Logo = () => {
   const data = useStaticQuery(graphql`
     query {
-      placeholderImage: file(relativePath: { eq: "images/havamal_bg.png" }) {
+      HavamalLogo: file(relativePath: { eq: "images/havamal_bg.png" }) {
         childImageSharp {
           fluid(maxWidth: 1200) {
             ...GatsbyImageSharpFluid_noBase64
@@ -16,11 +16,11 @@ const Logo = () => {
     }
   `)
 
-  if (!data?.placeholderImage?.childImageSharp?.fluid) {
+  if (!data?.HavamalLogo?.childImageSharp?.fluid) {
     return <div>Picture not found</div>
   }
 
-  return <Img fluid={data.placeholderImage.childImageSharp.fluid} />
+  return <Img fluid={data.HavamalLogo.childImageSharp.fluid} />
 }
 
 export default Logo
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,26 @@
-import React from "react"
+import React from 'react'
+import SEO from '../components/seo'
+import Logo from '../components/Logo'
+import styled from 'styled-components'
 
-import Logo from "../components/Logo"
-import SEO from "../components/seo"
+const LogoSection = styled.div`
+  // @media screen and (max-width:600px) {
+  //   .gatsby-image-wrapper {
+  //     width: 90%;
+  //     height: auto;
+  //   }
+  // }
+`;
+
 
 export default function IndexPage() {
   return (
     <>
       <SEO title="Home" />
-      <div className="flex flex-col items-center justify-center h-screen">
-        <Logo className="rounded-lg border" />
-        <h1 className="text-4xl font-bold text-blue-lighter">Hail, Hordes of Havamal!</h1>
-      </div>
+      <LogoSection className="flex flex-col items-center justify-center h-screen" >
+        <Logo />
+        <h1 className="text-6xl text-center font-pirata text-blue-lighter">Hail, Hordes of Havamal!</h1>
+      </LogoSection>
     </>
   )
 }
