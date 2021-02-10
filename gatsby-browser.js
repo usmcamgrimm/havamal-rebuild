0d219973992d7548df6cd3e9cbe4ec8d1b8347a5
Adrian Grimm <usmcamgrimm@gmail.com>
2021-02-10 12:02:31 -0500

    Working on the logo.....still

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,15 +1,15 @@
-import React from 'react'
-import Layout from './src/components/Layout'
+// import React from 'react'
+// import Layout from './src/components/Layout'
 
 import 'fontsource-roboto'
 import 'fontsource-pirata-one'
 import 'fontsource-grenze'
 import './src/styles/index.css'
 
-export function wrapPageElement({ element, props }) {
-  return (
-    <Layout {...props}>
-      {element}
-    </Layout>
-  )
-}
+// export function wrapPageElement({ element, props }) {
+//   return (
+//     <Layout {...props}>
+//       {element}
+//     </Layout>
+//   )
+// }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,26 +1,62 @@
 import React from 'react'
+import { useStaticQuery, graphql } from 'gatsby'
+import Img from 'gatsby-image'
+import Layout from '../components/Layout'
 import SEO from '../components/seo'
-import Logo from '../components/Logo'
 import styled from 'styled-components'
 
 const LogoSection = styled.div`
-  // @media screen and (max-width:600px) {
-  //   .gatsby-image-wrapper {
-  //     width: 90%;
-  //     height: auto;
-  //   }
-  // }
+  width: 90%;
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 2rem;
+
 `;
 
+const LogoWrapper = styled.div`
+  .gatsby-image-wrapper {
+    max-width: 1200px;
+    height: auto;
+  };
+  @media screen and (max-width: 500px) {
+    .gatsby-image-wrapper {
+      max-width: 400px;
+      height: auto;
+    }
+  }
+`;
 
 export default function IndexPage() {
+  const data = useStaticQuery(graphql`
+    query {
+      HavamalLogo: file(relativePath: { eq: "images/havamal_bg.png" }) {
+        childImageSharp {
+          fluid(maxWidth: 1200) {
+            ...GatsbyImageSharpFluid_noBase64
+            ...GatsbyImageSharpFluidLimitPresentationSize
+          }
+        }
+      }
+    }
+  `)
+
   return (
     <>
-      <SEO title="Home" />
-      <LogoSection className="flex flex-col items-center justify-center h-screen" >
-        <Logo />
-        <h1 className="text-6xl text-center font-pirata text-blue-lighter">Hail, Hordes of Havamal!</h1>
-      </LogoSection>
+      <Layout>
+        <SEO title="Home" />
+        <LogoSection>
+          <LogoWrapper>
+            <Img fluid={data.HavamalLogo.childImageSharp.fluid} />
+          </LogoWrapper>
+          <h1 className="text-5xl text-center font-pirata text-blue-lighter mx-6">Hail, Hordes of Havamal!</h1>
+        </LogoSection>
+      </Layout>
     </>
   )
 }
