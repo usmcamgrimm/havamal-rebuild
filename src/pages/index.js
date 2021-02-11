859897ceef8f3623f058b8ffe22cfc59bceeaf90
Adrian Grimm <usmcamgrimm@gmail.com>
2021-02-11 06:49:47 -0500

    Still working on the logo image

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,13 +3,25 @@ import { useStaticQuery, graphql } from 'gatsby'
 import Img from 'gatsby-image'
 import Layout from '../components/Layout'
 import SEO from '../components/seo'
+import styled from 'styled-components'
+
+const LogoSection = styled.div`
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+//   display: flex;
+//   flex-direction: column;
+//   justify-content: center;
+//   align-items: center;
+`;
 
 export default function IndexPage() {
   const data = useStaticQuery(graphql`
     query {
       HavamalLogo: file(relativePath: { eq: "images/havamal_bg.png" }) {
         childImageSharp {
-          fluid(maxWidth: 800) {
+          fluid(maxWidth: 1200) {
             ...GatsbyImageSharpFluid_noBase64
             ...GatsbyImageSharpFluidLimitPresentationSize
           }
@@ -22,10 +34,10 @@ export default function IndexPage() {
     <>
       <Layout>
         <SEO title="Home" />
-          <div className="container">
-            <Img fluid={data.HavamalLogo.childImageSharp.fluid} />
-            <h1 className="text-6xl text-center font-pirata text-blue-lighter">Hail, Hordes of Havamal!</h1>
-          </div>
+        <LogoSection>
+          <Img fluid={data.HavamalLogo.childImageSharp.fluid} />
+          <h1 className="text-4xl text-center font-pirata text-blue-lighter">Hail, Hordes of Havamal!</h1>
+        </LogoSection>
       </Layout>
     </>
   )
