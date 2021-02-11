b3dc095dad2288cef36767f8214744b959650808
Adrian Grimm <usmcamgrimm@gmail.com>
2021-02-11 06:15:08 -0500

    Start over without any styled components

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,40 +3,13 @@ import { useStaticQuery, graphql } from 'gatsby'
 import Img from 'gatsby-image'
 import Layout from '../components/Layout'
 import SEO from '../components/seo'
-import styled from 'styled-components'
-
-const LogoSection = styled.div`
-  position: fixed;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  padding: 2rem;
-
-`;
-
-const LogoWrapper = styled.div`
-  .gatsby-image-wrapper {
-    max-width: 1200px;
-    height: auto;
-  };
-  @media screen and (max-width: 500px) {
-    .gatsby-image-wrapper {
-      max-width: 400px;
-      height: auto;
-    }
-  }
-`;
 
 export default function IndexPage() {
   const data = useStaticQuery(graphql`
     query {
       HavamalLogo: file(relativePath: { eq: "images/havamal_bg.png" }) {
         childImageSharp {
-          fluid(maxWidth: 400) {
+          fluid(maxWidth: 800) {
             ...GatsbyImageSharpFluid_noBase64
             ...GatsbyImageSharpFluidLimitPresentationSize
           }
@@ -49,12 +22,10 @@ export default function IndexPage() {
     <>
       <Layout>
         <SEO title="Home" />
-        <LogoSection>
-          <LogoWrapper>
+          <div className="container">
             <Img fluid={data.HavamalLogo.childImageSharp.fluid} />
-          </LogoWrapper>
-          <h1 className="text-6xl text-center font-pirata text-blue-lighter">Hail, Hordes of Havamal!</h1>
-        </LogoSection>
+            <h1 className="text-6xl text-center font-pirata text-blue-lighter">Hail, Hordes of Havamal!</h1>
+          </div>
       </Layout>
     </>
   )
