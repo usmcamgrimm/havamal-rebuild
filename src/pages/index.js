5400b14789bfdc1011ad73c27c2f0fde22abefca
Adrian Grimm <usmcamgrimm@gmail.com>
2021-02-12 08:32:07 -0500

    Let's try art-directed images

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,23 +19,51 @@ const LogoSection = styled.div`
 export default function IndexPage() {
   const data = useStaticQuery(graphql`
     query {
-      HavamalLogo: file(relativePath: { eq: "images/havamal_bg.png" }) {
+      mobileLogo: file(relativePath: { eq: "images/havamal_bg.png" }) {
+        childImageSharp {
+          fluid(maxWidth: 400) {
+            ...GatsbyImageSharpFluid_noBase64
+            ...GatsbyImageSharpFluidLimitPresentationSize
+          }
+        }
+      }
+      tabletLogo: file(relativePath: { eq: "images/havamal_bg.png" }) {
+        childImageSharp {
+          fluid(maxWidth: 5500) {
+            ...GatsbyImageSharpFluid_noBase64
+            ...GatsbyImageSharpFluidLimitPresentationSize
+          }
+        }
+      }
+      desktopLogo: file(relativePath: { eq: "images/havamal_bg.png" }) {
         childImageSharp {
           fluid(maxWidth: 800) {
             ...GatsbyImageSharpFluid_noBase64
             ...GatsbyImageSharpFluidLimitPresentationSize
           }
         }
-      }
+      } 
     }
   `)
+
+  const logos = [
+    data.mobileLogo.childImageSharp.fluid,
+    {
+      ...data.tabletLogo.childImageSharp.fluid,
+      media: `(min-width: 601px)`,
+    },
+    {
+      ...data.desktopLogo.childImageSharp.fluid,
+      media: `(min-width: 769px)`,
+    },
+  ]
 
   return (
     <>
       <Layout>
         <SEO title="Home" />
         <LogoSection>
-          <Img fluid={data.HavamalLogo.childImageSharp.fluid} />
+          <Img fluid={logos} alt="Havamal logo" />
           <h1 className="text-4xl text-center font-pirata text-blue-lighter">Hail, Hordes of Havamal!</h1>
         </LogoSection>
       </Layout>
