2ae3c6cfbcf1f4d9098eb59093dbd82b7b84a38b
Adrian Grimm <usmcamgrimm@gmail.com>
2021-02-13 12:05:17 -0500

    Increase Saga text size

--- src/pages/saga.js
+++ src/pages/saga.js
@@ -54,16 +54,16 @@ export default function SagaPage({ data }) {
         The Havamal Saga
       </h1>
       <div className="container px-8 text-start">
-        <p className="font-grenze text-xl text-white">
+        <p className="font-grenze text-2xl text-white">
           Kjell Gilliusson and Lennie Spetze are hailed as the founders of Havamal. They formed the band in Stockholm, Sweden in 2016 and soon invited Björn Larsson. Andreas Herlogsson would join later, after the departure of Rodrigo Valenzuela. Sandra Wallo took over for Pontus Knutsson, until her departure in 2020.
         </p>
-        <p className="font-grenze text-xl text-white">
+        <p className="font-grenze text-2xl text-white">
           Havamal's crushing sound is derived from their wide and varying musical influences. Their mix of symphonic, brutal, melodic death metal is unique and makes themn stand out as masters of their craft. Their first release, an EP titled <strong>Call of The North</strong>, was released independently in 2017, and was an overwhelming success. Conducting raids throughout Sweden, Havamal was invited to play at Northern Blasphemy and the Wacken Metal Battle.
         </p>
-        <p className="font-grenze text-xl text-white">
+        <p className="font-grenze text-2xl text-white">
           Signed by Art Gates Records in 2019, the first full-length album, <strong>Tales From Yggdrasil</strong>, was released. Once again proving their musical prowess, Havamal unleashed many raids throughout their home country and spread their music. Hopefully we will soon see Havamal set sail on their longships and spread the raids across the globe.
         </p>
-        <p className="font-grenze text-xl text-white">
+        <p className="font-grenze text-2xl text-white">
           Allied once more with Art Gates Records, the hymn-weavers are returning to the studio to record their second full-length album. Look for this epic new album to be released in 2021!
         </p>
       </div>
