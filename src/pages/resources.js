6ca86d7672c9cb196199be9936a5c0ef2612b76b
Adrian Grimm <usmcamgrimm@gmail.com>
2021-02-21 12:43:15 -0500

    Finish the resources page

--- src/pages/resources.js
+++ src/pages/resources.js
@@ -9,6 +9,17 @@ import Contact from '../components/Contact'
 import Horde from '../resources/icons/horde'
 
 const FlexContainer = styled.div`
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: space-evenly;
+  margin: 0 auto;
+  padding: 0 2rem;
+  @media screen and (max-width: 768px) {
+    flex-direction: column;
+    justify-content: center;
+    margin: 0 auto;
+    left: 0;
+  }
   @media screen and (min-width: 1025px) {
     margin-top: -8rem;
   }
@@ -16,6 +27,20 @@ const FlexContainer = styled.div`
 
 const FlexSection = styled.div`
   width: 50%;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+  @media screen and (max-width: 768px) {
+    width: 100%;
+    p {
+      margin-top: 12px;
+      font-size: 1.5rem;
+    }
+    h2.banner {
+      padding-top: 8px;
+    }
+  }
 `;
 
 export default function ResourcesPage() {
@@ -43,18 +68,18 @@ export default function ResourcesPage() {
         fluid={data.logoImage.childImageSharp.fluid}
         className="container mx-auto my-16"
       />
-      <FlexContainer className="flex flex-row justify-evenly -mt-16">
-        <FlexSection className="flex flex-col justify-center">
+      <FlexContainer>
+        <FlexSection>
           <h2 className="font-grenze text-blue-lighter text-4xl text-center pt-12">Contact Havamal</h2>
           <Contact />
         </FlexSection>
-        <FlexSection className="flex flex-col items-center">
+        <FlexSection>
           <h2 className="font-grenze text-blue-lighter text-4xl text-center pt-12">Support Havamal</h2>
           <p className="font-grenze text-white text-3xl text-center mt-12">
             Click the link below to visit bandcamp for Official Havamal merch!
           </p>
           <a href="https://havamal.bandcamp.com/merch" className="font-grenze text-blue-lighter text-center text-3xl">Havamal Merch</a>
-          <h2 className="font-grenze text-white text-3xl text-center pt-16">Join the Horde</h2>
+          <h2 className="banner font-grenze text-white text-3xl text-center pt-16">Join the Horde</h2>
           <Horde />
         </FlexSection>
       </FlexContainer>
