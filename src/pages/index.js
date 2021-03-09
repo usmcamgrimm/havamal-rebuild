5f3e49ff531f476c3811f6c4e3c15aa8e517e421
Adrian Grimm <usmcamgrimm@gmail.com>
2021-03-09 12:30:13 -0500

    Try this instead

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,9 +5,8 @@ import SEO from '../components/seo'
 import BgImage from '../components/BgImage'
 import styled from 'styled-components'
 
-const FullscreenWrapper = styled.div`
-  height: 100vh;
-  maxWidth: 100%;
+const wrapper = styled.div`
+  height: 90vh;
 `;
 
 export default function IndexPage() {
@@ -55,14 +54,14 @@ export default function IndexPage() {
   return (
     <>
       <SEO title="Home" />
-        <FullscreenWrapper>
+        <wrapper>
           <BgImage>
             <div className="flex flex-col items-center justify-center h-screen">
               <Img fluid={logos} alt="Havamal logo" />
               <h1 className="text-4xl lg:text-6xl text-center font-pirata text-blue-lighter">Hail, Hordes of Havamal!</h1>
             </div>
           </BgImage>
-        </FullscreenWrapper>
+        </wrapper>
     </>
   )
 }
