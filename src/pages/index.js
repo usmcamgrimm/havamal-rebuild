445d8dc5c6430ff258651781a96bd85697bfe65d
Adrian Grimm <usmcamgrimm@gmail.com>
2021-03-10 11:51:53 -0500

    Temp fix

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,8 +5,9 @@ import SEO from '../components/seo'
 import BgImage from '../components/BgImage'
 import styled from 'styled-components'
 
-const wrapper = styled.div`
-  height: 90vh;
+const PageContainer = styled.div`
+  max-height: 94vh;
+  overflow: hidden;
 `;
 
 export default function IndexPage() {
@@ -54,14 +55,14 @@ export default function IndexPage() {
   return (
     <>
       <SEO title="Home" />
-        <wrapper>
+        <PageContainer>
           <BgImage>
             <div className="flex flex-col items-center justify-center h-screen">
               <Img fluid={logos} alt="Havamal logo" />
               <h1 className="text-4xl lg:text-6xl text-center font-pirata text-blue-lighter">Hail, Hordes of Havamal!</h1>
             </div>
           </BgImage>
-        </wrapper>
+        </PageContainer>
     </>
   )
 }
