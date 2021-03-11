8011b102e52df63babd308d975321188e4a37e35
Adrian Grimm <usmcamgrimm@gmail.com>
2021-03-11 11:47:14 -0500

    Correct h1 font size

--- src/pages/index.js
+++ src/pages/index.js
@@ -50,7 +50,7 @@ export default function IndexPage() {
       <SEO title="Home" />
       <div className="flex flex-col items-center justify-center h-screen">
         <Img fluid={logos} alt="Havamal logo" />
-        <h1 className="text-6xl text-center font-pirata text-blue-lighter">Hail, Hordes of Havamal!</h1>
+        <h1 className="text-4xl lg:text-6xl text-center font-pirata text-blue-lighter">Hail, Hordes of Havamal!</h1>
       </div>
     </>
   )
