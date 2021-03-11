aff1ec70403cc7ed157d967110a00c449a2ea77c
Adrian Grimm <usmcamgrimm@gmail.com>
2021-03-11 09:08:57 -0500

    Revert "Try this instead"
    This reverts commit 5f3e49ff531f476c3811f6c4e3c15aa8e517e421.

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,8 +5,9 @@ import SEO from '../components/seo'
 import BgImage from '../components/BgImage'
 import styled from 'styled-components'
 
-const wrapper = styled.div`
-  height: 90vh;
+const FullscreenWrapper = styled.div`
+  height: 100vh;
+  maxWidth: 100%;
 `;
 
 export default function IndexPage() {
@@ -54,14 +55,14 @@ export default function IndexPage() {
   return (
     <>
       <SEO title="Home" />
-        <wrapper>
+        <FullscreenWrapper>
           <BgImage>
             <div className="flex flex-col items-center justify-center h-screen">
               <Img fluid={logos} alt="Havamal logo" />
               <h1 className="text-4xl lg:text-6xl text-center font-pirata text-blue-lighter">Hail, Hordes of Havamal!</h1>
             </div>
           </BgImage>
-        </wrapper>
+        </FullscreenWrapper>
     </>
   )
 }
