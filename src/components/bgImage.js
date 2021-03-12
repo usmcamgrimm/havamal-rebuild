d645da5121326e384c42720d357081e82fc83a06
Adrian Grimm <usmcamgrimm@gmail.com>
2021-03-12 10:48:19 -0500

    Add component and query the bg image

---
+++ src/components/bgImage.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import { graphql, useStaticQuery } from 'gatsby'
+import BackgroundImage from 'gatsby-background-image'
+import styled from 'styled-components'
+
+const BGImage = styled(BackgroundImage)`
+  max-width: 100%;
+  height: auto;
+  z-index: -1;
+`;
+
+export default function bgImage ({ children }) {
+  const data = useStaticQuery(graphql`
+    query {
+      bgimg: file(relative path: { eq: "images/stage2.jpg" }) {
+        childImageSharp {
+          fluid(quality: 90){
+            ...GatsbyImageSharpFluid_withWebp
+          }
+        }
+      }
+    }
+  `)
+
+  const bg = data.bgimg.childImageSharp.fluid;
+
+  return (
+    <BGImage fluid={bg}>
+      {children}
+    </BGImage>
+  )
+}
