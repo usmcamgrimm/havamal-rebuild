21d4ed498ca3d2b2d80d202c169275ccf14ad106
Adrian Grimm <usmcamgrimm@gmail.com>
2021-03-17 12:00:49 -0400

    Added markdown package for Sanity

--- havamal/schemas/newsPost.js
+++ havamal/schemas/newsPost.js
@@ -40,7 +40,7 @@ export default {
       name: 'newsItem',
       title: 'News Item',
       description: 'Newsworthy posts from Havamal for updates and such',
-      type: 'text',
+      type: 'markdown',
     }
   ]
 }
