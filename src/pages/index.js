1057599689ff0dbe88f48c34e9e21e108c56d338
Adrian Grimm <usmcamgrimm@gmail.com>
2021-06-07 12:03:35 -0400

    Let's not use the static image for this since I'm trying to use ArtDirection

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,12 @@
 import React from 'react'
 import { StaticImage } from 'gatsby-plugin-image'
 import SEO from '../components/seo'
-//import Logo from '../components/Logo'
+import Logo from '../components/Logo'
 import BgImage from '../components/BgImage'
 import styled from 'styled-components'
 
 const TextShadow = styled.h1`
   text-shadow: 4px 4px 5px #000000;
-`;
-
-const LogoImage = styled.div`
-  @media screen and (max-width: 500px) {
-    max-width: 350px;
-    height: auto;
-  }
 `;
 
 export default function IndexPage() {
@@ -22,14 +15,7 @@ export default function IndexPage() {
       <SEO title="Home" />
       <BgImage>
         <div className="flex flex-col -mt-12 items-center justify-center h-screen">
-          <LogoImage>
-            <StaticImage
-              src="../resources/images/havamal_logo.png"
-              alt="Havamal logo"
-              layout="fullWidth"
-              placeholder="blurred"
-            />
-          </LogoImage>
+          <Logo />
           <TextShadow className="text-4xl lg:text-6xl text-center font-pirata text-blue-lighter">Hail, Hordes of Havamal!</TextShadow>
         </div>
       </BgImage>
