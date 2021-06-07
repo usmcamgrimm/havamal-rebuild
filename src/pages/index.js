93f0674439b703bf91cd844350f9aa81a1aee22c
Adrian Grimm <usmcamgrimm@gmail.com>
2021-06-07 18:52:15 -0400

    Still trying to get this to work

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
-import { StaticImage } from 'gatsby-plugin-image'
+import { graphql } from 'gatsby'
+import { GatsbyImage, getImage, withArtDirection } from 'gatsby-plugin-image'
 import SEO from '../components/seo'
-import Logo from '../components/Logo'
 import BgImage from '../components/BgImage'
 import styled from 'styled-components'
 
@@ -9,16 +9,47 @@ const TextShadow = styled.h1`
   text-shadow: 4px 4px 5px #000000;
 `;
 
-export default function IndexPage() {
+export default function IndexPage({ data }) {
+  const images = withArtDirection(getImage(data.defaultLogo), [
+    {
+      media: "(max-width: 500px)",
+      image: getImage(data.mobileLogo),
+    }
+  ])
+
   return (
     <>
       <SEO title="Home" />
       <BgImage>
         <div className="flex flex-col -mt-12 items-center justify-center h-screen">
-          <Logo />
+          <GatsbyImage
+            image={images}
+            alt={`Havamal logo`}
+          />
           <TextShadow className="text-4xl lg:text-6xl text-center font-pirata text-blue-lighter">Hail, Hordes of Havamal!</TextShadow>
         </div>
       </BgImage>
     </>
   )
 }
+
+export const query = graphql`
+  {
+    mobileLogo: file(relativePath: { eq: "images/havamal_logo_small.png" }) {
+      childImageSharp {
+        gatsbyImageData(
+          width: 350,
+          placeholder: BLURRED
+        )
+      }
+    }
+    defaultLogo: file(relativePath: { eq: "images/havamal_logo.png" }) {
+      childImageSharp {
+        gatsbyImageData(
+          width: 800,
+          placeholder: BLURRED
+        )
+      }
+    }
+  }
+`
