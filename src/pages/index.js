1d1ad937f738021ed83d4e9150a92222363d9a3d
Adrian Grimm <usmcamgrimm@gmail.com>
2021-06-09 12:05:13 -0400

    I'd shit myself if this had actually worked

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,30 +1,38 @@
 import React from 'react'
-import { graphql } from 'gatsby'
-import { GatsbyImage, getImage, withArtDirection } from 'gatsby-plugin-image'
+// import { graphql } from 'gatsby'
+// import { GatsbyImage, getImage, withArtDirection } from 'gatsby-plugin-image'
+import { StaticImage } from 'gatsby-plugin-image'
 import SEO from '../components/seo'
 import BgImage from '../components/BgImage'
 import styled from 'styled-components'
+
+import '../styles/Logo.css'
 
 const TextShadow = styled.h1`
   text-shadow: 4px 4px 5px #000000;
 `;
 
-export default function IndexPage({ data }) {
-  const images = withArtDirection(getImage(data.defaultLogo), [
-    {
-      media: "(max-width: 500px)",
-      image: getImage(data.mobileLogo),
-    }
-  ])
+export default function IndexPage() {
+//   const images = withArtDirection(getImage(data.defaultLogo), [
+//     {
+//       media: "(max-width: 500px)",
+//       image: getImage(data.mobileLogo),
+//     }
+//   ])
 
   return (
     <>
       <SEO title="Home" />
       <BgImage>
         <div className="flex flex-col -mt-12 items-center justify-center h-screen">
-          <GatsbyImage
+          {/* <GatsbyImage
             image={images}
             alt={`Havamal logo`}
+          /> */}
+          <StaticImage
+            src="../resources/images/havamal_logo.png"
+            layout="fullWidth"
+            alt="Havamal logo"
           />
           <TextShadow className="text-4xl lg:text-6xl text-center font-pirata text-blue-lighter">Hail, Hordes of Havamal!</TextShadow>
         </div>
@@ -33,23 +41,23 @@ export default function IndexPage({ data }) {
   )
 }
 
-export const query = graphql`
-  {
-    mobileLogo: file(relativePath: { eq: "images/havamal_logo_small.png" }) {
-      childImageSharp {
-        gatsbyImageData(
-          width: 350,
-          placeholder: BLURRED
-        )
-      }
-    }
-    defaultLogo: file(relativePath: { eq: "images/havamal_logo.png" }) {
-      childImageSharp {
-        gatsbyImageData(
-          width: 800,
-          placeholder: BLURRED
-        )
-      }
-    }
-  }
-`
+// export const LogoQuery = graphql`
+//   query {
+//     mobileLogo: file(relativePath: { eq: "images/havamal_logo_test.png" }) {
+//       childImageSharp {
+//         gatsbyImageData(
+//           width: 350,
+//           placeholder: BLURRED
+//         )
+//       }
+//     }
+//     defaultLogo: file(relativePath: { eq: "images/havamal_logo.png" }) {
+//       childImageSharp {
+//         gatsbyImageData(
+//           width: 800,
+//           placeholder: BLURRED
+//         )
+//       }
+//     }
+//   }
+// `
