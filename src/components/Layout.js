47d0a76fa31b14ad9d0ac25cc6c52a4067507e35
Adrian Grimm <usmcamgrimm@gmail.com>
2021-07-24 12:19:42 -0400

    Make it full screen

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -3,12 +3,12 @@ import '../styles/layout.css'
 
 import { css } from '@emotion/react'
 
-
 export default function Layout({ children }) {
   return (
     <div 
       css={css`
         background-color: #070707;
+        min-height: 100vh;
       `}
     >
       {children}
