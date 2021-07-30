f1963b675afff4db64d253189be3fbf969075228
Adrian Grimm <usmcamgrimm@gmail.com>
2021-07-30 19:56:41 -0400

    Add links to the images

--- src/components/LinkLayout.js
+++ src/components/LinkLayout.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { StaticImage } from 'gatsby-plugin-image'
+import { css } from '@emotion/react'
 import styled from '@emotion/styled'
 
 const LinkGrid = styled.div`
@@ -35,52 +36,106 @@ export default function LinkLayout() {
     <>
       <LinkGrid>
         <LinkItem>
-          <StaticImage
-            src="../resources/icons/bandcamp.png"
-            alt="bandcamp icon"
-            layout="constrained"
-            width={50}
-          />
-          <StaticImage
-            src="../resources/icons/bandbond.png"
-            alt="bandbond icon"
-            layout="constrained"
-            width={50}
-          />
-          <StaticImage
-            src="../resources/icons/facebook.png"
-            alt="facebook icon"
-            layout="constrained"
-            width={50}
-          />
-          <StaticImage
-            src="../resources/icons/instagram.png"
-            alt="instagram icon"
-            layout="constrained"
-            width={50}
-          />
-          <StaticImage
-            src="../resources/icons/spotify.png"
-            alt="spotify icon"
-            layout="constrained"
-            width={50}
-          />
-          <StaticImage
-            src="../resources/icons/youtube.png"
-            alt="youtube icon"
-            layout="constrained"
-            width={50}
-          />
+          <a 
+            href="https://havamal.bandcamp.com/" 
+            target="_blank" 
+            rel="noreferrer" 
+            aria-label="Bandcamp"
+            css={css`cursor: crosshair`}
+          >
+            <StaticImage
+              src="../resources/icons/bandcamp.png"
+              alt="bandcamp icon"
+              layout="constrained"
+              width={50}
+            />
+          </a>
+          <a 
+            href="https://bandbond.com/" 
+            target="_blank" 
+            rel="noreferrer" 
+            aria-label="Bandbond"
+            css={css`cursor: crosshair;`}
+          >
+            <StaticImage
+              src="../resources/icons/bandbond.png"
+              alt="bandbond icon"
+              layout="constrained"
+              width={50}
+            />
+          </a>
+          <a 
+            href="https://www.facebook.com/Havamalofficial" 
+            target="_blank" 
+            rel="noreferrer" 
+            aria-label="Facebook"
+            css={css`cursor: crosshair`}
+          >
+            <StaticImage
+              src="../resources/icons/facebook.png"
+              alt="facebook icon"
+              layout="constrained"
+              width={50}
+            />
+          </a>
+          <a href="https://www.instagram.com/officialhavamal/" 
+            target="_blank" 
+            rel="noreferrer" 
+            aria-label="Instagram"
+            css={css`cursor: crosshair`}
+          >
+            <StaticImage
+              src="../resources/icons/instagram.png"
+              alt="instagram icon"
+              layout="constrained"
+              width={50}
+            />
+          </a>
+          <a
+            href="https://open.spotify.com/artist/6ldp4tSRbPDs5VA2w9KXmD?si=2RV3id28Rems8KVYhqp4BQ&dl_branch=1"
+            target="_blank" 
+            rel="noreferrer" 
+            aria-label="Havamal on Spotify"
+            css={css`cursor: crosshair`}
+          >
+            <StaticImage
+              src="../resources/icons/spotify.png"
+              alt="spotify icon"
+              layout="constrained"
+              width={50}
+            />
+          </a>
+          <a href="https://www.youtube.com/channel/UCmGfesgQAVTgpJqGf9dt3wA/videos" 
+            target="_blank" 
+            rel="noreferrer" 
+            aria-label="YouTube"
+            css={css`cursor: crosshair`}
+          >
+            <StaticImage
+              src="../resources/icons/youtube.png"
+              alt="youtube icon"
+              layout="constrained"
+              width={50}
+            />
+          </a>
         </LinkItem>
       </LinkGrid>
       <LinkGrid>
         <BannerLinks>
-          <StaticImage
-            src="../resources/icons/AGR.png"
-            alt="art gates records icon"
-            layout="fixed"
-            width={400}
-          />
+          <a 
+            href="https://artgatesrecords.com/"
+            target="_blank"
+            rel="noreferrer"
+            aria-label="Art Gates Records"
+            css={css`cursor: crosshair`}
+          >
+            <StaticImage
+              src="../resources/icons/AGR.png"
+              alt="art gates records icon"
+              layout="fixed"
+              width={400}
+            />
+          </a>
         </BannerLinks>
       </LinkGrid>
     </>
