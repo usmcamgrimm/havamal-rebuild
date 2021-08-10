5f7837c0dd5b60abf7a32861065551c421118d2e
Adrian Grimm <usmcamgrimm@gmail.com>
2021-08-10 11:55:26 -0400

    Configuring the media page

---
+++ src/pages/photos.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import Layout from '../components/Layout'
+import MediaGallery from '../components/MediaGallery'
+import SimpleReactLightbox from 'simple-react-lightbox'
+
+import { css } from '@emotion/react'
+
+export default function PhotosPage() {
+  return (
+    <Layout>
+      <h1 css={css`
+        font-family: grenze;
+        font-size: 2rem;
+        color: #004b87;
+        text-align: center;
+        margin-top: 4rem;
+        margin-bottom: 4rem;
+      `}>The Hymns of Havamal</h1>
+
+      <SimpleReactLightbox>
+        <MediaGallery />
+      </SimpleReactLightbox>
+    </Layout>
+  )
+}
