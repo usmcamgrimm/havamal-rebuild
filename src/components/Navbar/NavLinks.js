45bdab4251bfa5f3cde36defef5176a0513bee00
Adrian Grimm <usmcamgrimm@gmail.com>
2021-08-13 11:47:24 -0400

    Fix the Video link to point to an anchor on the music page

--- src/components/Navbar/NavLinks.js
+++ src/components/Navbar/NavLinks.js
@@ -1,13 +1,32 @@
 import React from 'react'
 import { Link } from 'gatsby'
 import styled from '@emotion/styled'
+import { css } from '@emotion/react'
 
 export default function NavLinks() {
   return(
     <>
       <NavLink to="/">Home</NavLink>
       <NavLink to="/music">Music</NavLink>
-      <NavLink to="/video">Videos</NavLink>
+      <a href="../music#videoList"
+        css={css`
+          color: #ffcd00;
+          font-family: grenze;
+          font-size: 1.25rem;
+          text-decoration: none;
+          padding: 8px 1.25rem;
+          :hover {
+            color: #ffffff;
+          }
+          @media screen and (max-width: 768px) {
+            padding: 12px 0;
+            font-size: 1.75rem;
+            z-index: 5;
+          }
+        `}
+      >
+        Videos
+      </a>
       <NavLink to="/photos">Photos</NavLink>
       <NavLink to="/saga">Saga</NavLink>
       <NavLink to="/raids">Raids</NavLink>
