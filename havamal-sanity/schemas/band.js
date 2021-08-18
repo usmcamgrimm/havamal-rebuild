96aef84ca22238840237e2a249cfa2cf4d411c12
Adrian Grimm <usmcamgrimm@gmail.com>
2021-08-18 11:55:29 -0400

    Add schemas for Sanity

---
+++ havamal-sanity/schemas/band.js
@@ -0,0 +1,34 @@
+export default {
+  name: 'band',
+  title: 'HymnWeavers',
+  type: 'document',
+  fields: [
+    {
+      name: 'name',
+      title: 'Name',
+      type: 'string',
+    },
+    {
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        source: 'name',
+        maxLength: 30,
+      },
+    },
+    {
+      name: 'image',
+      title: 'Photo',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    },
+    {
+      name: 'instrument',
+      title: 'Instrument',
+      type: 'string',
+    },
+  ],
+}
---
+++ havamal-sanity/schemas/newsPost.js
@@ -0,0 +1,46 @@
+export default {
+  name: 'newsPost',
+  title: 'Havamal News',
+  type: 'document',
+  fields: [
+    {
+      name: 'name',
+      title: 'Title',
+      type: 'string'
+    },
+    {
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        source: 'name',
+        maxLength: 100,
+      },
+    },
+    {
+      name: 'postDate',
+      title: 'Date of Post',
+      type: 'date'
+    },
+    {
+      name: 'postAuthor',
+      title: 'Authored By',
+      type: 'string',
+      description: 'Shows if post was written by Havamal or someone else',
+    },
+    {
+      name: 'image',
+      title: 'Image',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    },
+    {
+      name: 'newsItem',
+      title: 'News Item',
+      description: 'Newsworthy posts from Havamal for updates and such',
+      type: 'text'
+    }
+  ]
+}
