c31523ff9400ff187d0ab3a520739cdb8c825ed1
Adrian Grimm <usmcamgrimm@gmail.com>
2021-08-27 12:03:45 -0400

    Text formatting

--- src/pages/saga.js
+++ src/pages/saga.js
@@ -36,6 +36,18 @@ const HW = styled.div`
     text-align: center;
   }
 `;
+const SagaContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 80%;
+  text-align: start;
+`;
+const SagaText = styled.p`
+  font-family: grenze;
+  color: #ffffff;
+  font-size: 1.2rem;
+`;
 
 export default function Saga({ data }) {
   const hymnweavers = data.hymnweavers.nodes
@@ -63,20 +75,35 @@ export default function Saga({ data }) {
       </section>
 
       <section id="havamal-saga">
-        <SagaText>
-          <p>
+        <h1 css={css`
+          font-family: grenze;
+          font-size: 2rem;
+          color: #004b87;
+          text-align: center;
+          margin-top: 4rem;
+          margin-bottom: 4rem;
+        `}>The Havamal Saga</h1>
+        <SagaContainer>
+          <SagaText>
             Kjell Gilliusson and Lennie Spetze are hailed as the founders of Havamal. They formed the band in Stockholm, Sweden in 2016 and soon invited Björn Larsson. Andreas Herlogsson would join later, after the departure of Rodrigo Valenzuela. Sandra Wallo took over for Pontus Knutsson, until her departure in 2020. Now hail Tino Vesanen on bass!
-          </p>
-          <p>
+          </SagaText>
+          <SagaText>
             Havamal's crushing sound is derived from their wide and varying musical influences. Their mix of symphonic, brutal, melodic death metal is unique and makes themn stand out as masters of their craft. Their first release, an EP titled <strong>Call of The North</strong>, was released independently in 2017, and was an overwhelming success. Conducting raids throughout Sweden, Havamal was invited to play at Northern Blasphemy and the Wacken Metal Battle.
-          </p>
-          <p>
+          </SagaText>
+          <SagaText>
             Signed by Art Gates Records in 2019, the first full-length album, <strong>Tales From Yggdrasil</strong>, was released. Once again proving their musical prowess, Havamal unleashed many raids throughout their home country and spread their music. Hopefully we will soon see Havamal set sail on their longships and spread the raids across the globe.
-          </p>
-          <p>
+          </SagaText>
+          <SagaText>
             Havamal's second full-length album, "The Shadow Chapter", is out NOW!
-          </p>
-        </SagaText>
+          </SagaText>
+          <h1 css={css`
+            font-family: Norse;
+            font-size: 3rem;
+            color: #004b87;
+            text-align: center;
+            margin-top: 4rem;
+        `}>Hail Havamal!</h1>
+        </SagaContainer>
       </section>
     </Layout>
   )
