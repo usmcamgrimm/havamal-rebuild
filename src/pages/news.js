ef182cc2f00dfe6f64821437caf1c5b4eb858d57
Adrian Grimm <usmcamgrimm@gmail.com>
2021-09-08 12:02:11 -0400

    Adding the news page

---
+++ src/pages/news.js
@@ -0,0 +1,49 @@
+import React from 'react'
+import { graphql } from 'gatsby'
+import Layout from '../components/layout'
+
+import { css } from '@emotion/react'
+import styled from '@emotion/styled'
+
+export default function news() {
+  const news = data.news.nodes;
+  return (
+    <Layout>
+      <h1 css={css`
+        font-family: Norse;
+        font-size: 3rem;
+        font-weight: normal;
+        color: #66add9;
+        text-align: center;
+        margin-top: 4rem;
+        margin-bottom: 4rem;
+      `}>Updates From Havamal</h1>
+
+      <NewsPosts news={news} />
+    </Layout>
+  )
+}
+
+export const query = graphql;`
+  query NewsQuery {
+    news: allSanityNewsPost(sort: {fields: postDate, order: DESC}) {
+      nodes {
+        name
+        id
+        slug {
+          current
+        }
+        postDate
+        postAuthor
+        newsItem
+        image {
+          asset {
+            fluid(maxWidth: 400) {
+              ...GatsbySanityImageFluid
+            }
+          }
+        }
+      }
+    }
+  }
+`;
