456d15950a18f2a3c3473e5f452d4f8869225d29
Adrian Grimm <usmcamgrimm@gmail.com>
2021-09-08 18:47:10 -0400

    Add a function to generate pages from Sanity posts

---
+++ gatsby-node.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const graphql = require('gatsby').graphql
+
+async function newsPostPages({ graphql, actions }) {
+  const newsTemplate = path.resolve('./src/templates/News.js');
+
+  const { data } = await graphql(`
+    query {
+      updates: allSanityNerwsPost {
+        nodes {
+          name
+          slug {
+            current
+          }
+        }
+      }
+    }
+  `);
+  data.updates.nodes.forEach((newsPost) => {
+    actions.createPage({
+      path: `post/${newsPost.slug.current}`,
+      component: newsTemplate,
+      context: {
+        slug: newsPost.slug.current,
+      }
+    })
+  })
+}
+
+exports.createPages = async function (params) {
+  await Promise, all([
+    newsPostPages(params),
+  ])
+}
