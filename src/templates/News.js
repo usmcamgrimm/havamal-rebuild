cdf1184aecec0c76bfa9fda7ca8954e456b81b0d
Adrian Grimm <usmcamgrimm@gmail.com>
2021-09-09 14:40:17 -0400

    Add the template for news items

---
+++ src/templates/News.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import { graphql } from 'gatsby'
+import GatsbyImage from 'gatsby-plugin-image'
+
+import styled from '@emotion/styled'
+
+export default function NewsItemPage({ data: { news }}) {
+  return (
+    <>
+      <h2>{news.name}</h2>
+      <p>`By: ${news.postAuthor}`</p>
+      <div>
+        <div>
+          <GatsbyImage fluid={news.image.asset.fluid} />
+          <p>{news.newsItem}</p>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export const query = graphql`
+  query($slug: String!) {
+    news: sanityNewsPost(slug: { current: { eq: $slug } }) {
+      name
+      id
+      postAuthor
+      image {
+        asset {
+          fluid(maxWidth: 500) {
+            ...GatsbySanityImageFluid
+          }
+        }
+      }
+      newsItem
+    }
+  }
+`;
