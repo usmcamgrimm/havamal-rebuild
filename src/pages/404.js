dbd2664cc88736889351c49b69951ae55516b478
Adrian Grimm <usmcamgrimm@gmail.com>
2021-09-19 14:47:01 -0400

    404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,54 +1,20 @@
-import * as React from "react"
-import { Link } from "gatsby"
+import React from 'react'
+import Layout from '../components/layout'
 
-// styles
-const pageStyles = {
-  color: "#232129",
-  padding: "96px",
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
+import { css } from '@emotion/react'
 
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
-
-// markup
-const NotFoundPage = () => {
+export default function NotFoundPage() {
   return (
-    <main style={pageStyles}>
-      <title>Not found</title>
-      <h1 style={headingStyles}>Page not found</h1>
-      <p style={paragraphStyles}>
-        Sorry{" "}
-        <span role="img" aria-label="Pensive emoji">
-          😔
-        </span>{" "}
-        we couldn’t find what you were looking for.
-        <br />
-        {process.env.NODE_ENV === "development" ? (
-          <>
-            <br />
-            Try creating a page in <code style={codeStyles}>src/pages/</code>.
-            <br />
-          </>
-        ) : null}
-        <br />
-        <Link to="/">Go home</Link>.
-      </p>
-    </main>
+    <Layout>
+      <h1 css={css`
+        font-family: Norse;
+        font-size: 3rem;
+        font-weight: normal;
+        color: #66add9;
+        text-align: center;
+        margin-top: 4rem;
+        margin-bottom: 4rem;
+      `}>There's nothing here!</h1>
+    </Layout>
   )
 }
-
-export default NotFoundPage
